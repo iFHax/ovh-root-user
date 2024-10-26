5b53051900f2efca60ec82a261cd8b669412e0f4
iFHax <ifhax@proton.me>
2024-10-27 07:07:56 +0800

    Update ovh-root.sh

--- ovh-root.sh
+++ ovh-root.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
- 
+
 get_ip() {
     ip=$(hostname -I | awk '{print $1}')
     echo "$ip"
 }
-
-# Enable root login for SSH
+ 
 echo "Modifying SSH configuration to allow root login..."
 sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
- 
-echo "Setting root password..."
-read -s -p "Enter new root password: " root_password
-echo
-read -s -p "Confirm new root password: " root_password_confirm
-echo
 
-# Check if passwords match
+echo "Setting root password..."
+read -p "Enter new root password: " root_password
+echo
+read -p "Confirm new root password: " root_password_confirm
+echo
+ 
 if [ "$root_password" != "$root_password_confirm" ]; then
     echo "Passwords do not match. Exiting..."
     exit 1
 fi
-
+ 
 echo "root:$root_password" | sudo chpasswd
-
 
 echo "Restarting SSH service..."
 sudo systemctl restart sshd
@@ -31,6 +28,6 @@ echo "Root login via SSH is now enabled."
 echo "Username: root"
 echo "Password: $root_password"
 echo "IP Address: $(get_ip)"
-
+ 
 sleep 10
 reboot
